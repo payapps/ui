b102403415a4f7627267af5542844c2a0bdd335b
Ben Daniel <3base1@gmail.com>
2021-04-19 09:30:57 +1000

    Passing ThemeProvider as global decorator in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,14 @@
+import React from 'react'
+import { ThemeProvider } from '../src/components/ThemeProvider'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider>
+      <Story />
+    </ThemeProvider>
+  ),
+];
